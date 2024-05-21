f899c825a643ccf5a0f1be0b22d2d63674a4b322
RathomBoi <jessada.srm@gmail.com>
2024-05-21 23:52:47 +0700

    define all module in root main

--- src/main/main.tf
+++ src/main/main.tf
@@ -1,8 +1,22 @@
-module "s3_website_hoster_bucket" {
+module "s3_website_bucket" {
   source = "../modules/s3/website_bucket"
 }
 
-module "s3_website_logger_bucket" {
+module "s3_website_log_bucket" {
   source = "../modules/s3/logging_bucket"
 }
 
+module "cloudfront" {
+  source = "../modules/cloudfront"
+  origin_domain_name = module.s3_website_bucket.website_bucket_endpoint
+  website_bucket = module.s3_website_bucket
+  log_bucket = module.s3_website_log_bucket
+  depends_on = [ module.s3_website_bucket, module.s3_website_logger_bucket ]
+}
+
+module "route53"{
+  source = "../modules/route53"
+  depends_on = [ module.cloudfront ]
+  cloudfront_distribution_domain_name = module.cloudfront.cloudfront_distribution_domain_name
+  cloudfront_distribution_hosted_zone_id = module.cloudfront.cloudfront_distribution_hosted_zone_id
+}
--- src/main/outputs.tf
+++ src/main/outputs.tf
@@ -1,7 +1,7 @@
 output "s3_website_hoster_bucket" {
-  value = module.s3_website_hoster_bucket.id
+  value = module.s3_website_bucket
 }
 
 output "s3_website_logger_bucket" {
-  value = module.s3_website_logger_bucket.id
+  value = module.s3_website_logger_bucket
 }
