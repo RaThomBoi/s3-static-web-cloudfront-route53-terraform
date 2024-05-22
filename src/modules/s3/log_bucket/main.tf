4a23375f892050926dd56f0761142b3161a3503a
RathomBoi <jessada.srm@gmail.com>
2024-05-22 20:47:51 +0700

    add log bucket policy to allow cloudfront distribution to logging data in bucket

--- src/modules/s3/log_bucket/main.tf
+++ src/modules/s3/log_bucket/main.tf
@@ -1,9 +1,9 @@
-resource "aws_s3_bucket" "website_log_bucket_name" {
+resource "aws_s3_bucket" "website_log_bucket" {
     bucket = var.website_log_bucket_name
 }
 
 resource "aws_s3_bucket_public_access_block" "website_log_bucket_public_access_configuration" {
-    bucket = aws_s3_bucket.website_log_bucket_name
+    bucket = aws_s3_bucket.website_log_bucket.id
 
     block_public_acls = true
     ignore_public_acls = true
@@ -11,6 +11,31 @@ resource "aws_s3_bucket_public_access_block" "website_log_bucket_public_access_c
     restrict_public_buckets = true
 }
 
-output "website_bucket" {
-  value = aws_s3_bucket.website_log_bucket_name
+
+# bucket policy to allow cloudfront distribution logging data into this bucket
+data "aws_iam_policy_document" "log_bucket_policy" {
+  statement {
+    sid = "AllowCloudFrontServicePrincipal"
+    effect = "Allow"
+    principals {
+      type = "Service"
+      identifiers = ["cloudfront.amazonaws.com"]
+    }
+    actions = ["s3:PutObject"]
+    resources = ["${aws_s3_bucket.website_log_bucket.arn}/*"]
+    condition {
+      test = "StringEquals"
+      variable = "AWS:SourceArn"
+      values = [var.cloudfront_distribution_arn]
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "log_bucket_policy" {
+  bucket = aws_s3_bucket.website_log_bucket.id
+  policy = data.aws_iam_policy_document.log_bucket_policy.json
+}
+
+output "website_bucket_id" {
+  value = aws_s3_bucket.website_log_bucket.id
 }
--- src/modules/s3/log_bucket/variables.tf
+++ src/modules/s3/log_bucket/variables.tf
@@ -2,5 +2,9 @@ variable "website_log_bucket_name" {
   default = "jessada-corp-website_log-bucket"
 }
 
-
+variable "cloudfront_distribution_arn" {
+  description = "The ARN of the CloudFront distribution"
+  type = string
+  default = ""
+}
 
