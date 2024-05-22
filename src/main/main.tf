7b480c034691356cf5331517685c4ca1a74dbb31
RathomBoi <jessada.srm@gmail.com>
2024-05-22 20:48:42 +0700

    align module referencing based on revised configuration on each module.

--- src/main/main.tf
+++ src/main/main.tf
@@ -1,17 +1,18 @@
 module "s3_website_bucket" {
   source = "../modules/s3/website_bucket"
+  cloudfront_distribution_arn = module.cloudfront.distribution_arn
 }
 
 module "s3_website_log_bucket" {
-  source = "../modules/s3/logging_bucket"
+  source = "../modules/s3/log_bucket"
+  cloudfront_distribution_arn = module.cloudfront.distribution_arn
 }
 
 module "cloudfront" {
   source = "../modules/cloudfront"
   origin_domain_name = module.s3_website_bucket.website_bucket_endpoint
   website_bucket = module.s3_website_bucket
-  log_bucket = module.s3_website_log_bucket
-  depends_on = [ module.s3_website_bucket, module.s3_website_logger_bucket ]
+  log_bucket_id = module.s3_website_log_bucket.website_bucket_id
 }
 
 module "route53"{
--- src/main/outputs.tf
+++ src/main/outputs.tf
@@ -3,5 +3,5 @@ output "s3_website_hoster_bucket" {
 }
 
 output "s3_website_logger_bucket" {
-  value = module.s3_website_logger_bucket
+  value = module.s3_website_log_bucket
 }
