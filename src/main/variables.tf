794535054efc129b1c7eb3b3a35e3361b105ac3e
RathomBoi <jessada.srm@gmail.com>
2024-05-22 21:13:54 +0700

    add var

--- src/main/variables.tf
+++ src/main/variables.tf
@@ -9,3 +9,12 @@ variable "website_log_bucket_name" {
 variable "region" {
 default = "ap-southeast-1"
 }
+
+variable "aws_access_key_id" {
+  type = string
+  default = ""
+}
+variable "aws_secret_access_key" {
+  type = string
+  default = ""
+}
